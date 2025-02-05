66ea113b67185bea87a9e5e3745d0ff0ad850dca
IsurikaDaniel <d.nethmiisurika@gmail.com>
2025-02-05 20:38:40 +0530

    scrollbar changes added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Admin from './Admin panel/AdminView'
 import reportWebVitals from './reportWebVitals';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -9,7 +10,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Admin/>
   </React.StrictMode>
 );
 
