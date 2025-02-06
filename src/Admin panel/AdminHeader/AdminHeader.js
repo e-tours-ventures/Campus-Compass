959712ee9a66538860edd8e7bc8f329c428fdfef
IsurikaDaniel <d.nethmiisurika@gmail.com>
2025-02-06 23:24:12 +0530

    nav bar impliemtation

--- src/Admin panel/AdminHeader/AdminHeader.js
+++ src/Admin panel/AdminHeader/AdminHeader.js
@@ -1,36 +1,25 @@
-import React, { useState, useRef, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, { useState } from "react";
 import "./AdminHeader.css";
 import logo from "../../Home/assets/images/logo.png";
 
-
-
 function AdminHeader() {
-
-  const navRef = useRef(null);
-
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <header className="header">
-      <div className="container">
+      <div className="navbar-container">
         <div className="logo">
           <img src={logo} alt="Campus Compass Logo" className="logo-image" />
           <span className="logo-text">Campus Compass</span>
         </div>
 
-        <div className="hamburger-menu">
-          &#9776;
-        </div>
-
-        <nav className="nav-links">
+        <nav className="nav">
           <ul>
             <li>Home</li>
             <li>Student Info</li>
             <li>Courses</li>
           </ul>
         </nav>
-
-        
       </div>
     </header>
   );
--- src/Admin panel/AdminView.js
+++ src/Admin panel/AdminView.js
@@ -17,10 +17,43 @@ function AdminView() {
     return (
         <>
             {/* <div className="row"> */}
-                <AdminHeader />
+            <AdminHeader />
 
-                {/* <AdminHeader /> */}
-                <div className="row">
+            <div className="search-container">
+                <div className="row search-row">
+                    <div className="col-md-8 col-12">
+                        <form className="form">
+                            <label htmlFor="search">
+                                <input className="input1" type="text" required placeholder="Search here" id="search" />
+                                <div className="fancy-bg"></div>
+                                <div className="search">
+                                    <svg viewBox="0 0 24 24" aria-hidden="true" className="search-icon">
+                                        <g>
+                                            <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+                                        </g>
+                                    </svg>
+                                </div>
+                                <button className="close-btn" type="reset">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                        <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path>
+                                    </svg>
+                                </button>
+                            </label>
+                        </form>
+                    </div>
+
+                    <div className="col-md-4 col-12">
+                        <div className="auth-buttons">
+                            <button className="addStu-btn">Add new Student</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            {/* search bar */}
+
+            {/* <div className="row serch-bar">
                     <div className="col-6 col-md-6 mb-3">
 
                         <form className="form">
@@ -45,42 +78,42 @@ function AdminView() {
 
                     <div className="col-6 col-md-6 mb-3">
                         <div className="auth-buttons">
-                            <button className="login-btn" >Add new Student</button>
-                            {/* <button className="signup-btn" >Sign Up</button> */}
+                            <button className="addStu-btn" >Add new Student</button>
                         </div>
                     </div>
-                </div>
-                {/* Table for Students & Feedback */}
-                <div className="container">
-                    <div className="mask">
-                        <table className="table table-bordered table-hover">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                    <th>Feedback</th>
-                                    <th>Actions</th>
+                </div> */}
+
+            {/* Table for Students & Feedback */}
+            <div className="container">
+                <div className="mask">
+                    <table className="table table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Feedback</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {students.map((student, index) => (
+                                <tr key={index}>
+                                    <td>{student.id}</td>
+                                    <td>{student.name}</td>
+                                    <td>{student.email}</td>
+                                    <td>{student.feedback}</td>
+                                    <td>
+                                        <button className="btn btn-danger">
+                                            <i className="bi bi-trash-fill"></i>
+                                        </button>
+                                    </td>
                                 </tr>
-                            </thead>
-                            <tbody>
-                                {students.map((student, index) => (
-                                    <tr key={index}>
-                                        <td>{student.id}</td>
-                                        <td>{student.name}</td>
-                                        <td>{student.email}</td>
-                                        <td>{student.feedback}</td>
-                                        <td>
-                                            <button className="btn btn-danger">
-                                                <i className="bi bi-trash-fill"></i>
-                                            </button>
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
+                            ))}
+                        </tbody>
+                    </table>
                 </div>
+            </div>
             {/* </div> */}
         </>
     );
